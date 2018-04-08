5e04a5248162c29a35e4bd64d3be81fed6885920
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-08 20:38:43 +0100

    first commit

---
+++ src/main/java/com/amarinperez/utils/Exceptions.java
@@ -0,0 +1,21 @@
+package com.amarinperez.utils;
+
+import java.util.concurrent.Callable;
+
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+public class Exceptions {
+    private static <T> void expectException(Callable<T> statement, Class<? extends Exception> expectedException,
+                                            String expectedMessage, String failureMessage) {
+        try {
+            statement.call();
+            fail(failureMessage);
+        } catch (Exception e) {
+            assertEquals(expectedException, e.getClass());
+            assertThat(e.getMessage(), containsString(expectedMessage));
+        }
+    }
+}
