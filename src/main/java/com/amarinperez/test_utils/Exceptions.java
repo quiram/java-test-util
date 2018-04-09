401520efdac99910e58fc967b05f56edb8007b75
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-09 12:39:24 +0100

    change package to avoid clashes with quiram/java-utils

--- src/main/java/com/amarinperez/test_utils/Exceptions.java
+++ src/main/java/com/amarinperez/test_utils/Exceptions.java
@@ -1,11 +1,9 @@
-package com.amarinperez.utils;
+package com.amarinperez.test_utils;
 
 import java.util.concurrent.Callable;
 
 import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class Exceptions {
     public static <T> void expectException(Callable<T> statement, Class<? extends Exception> expectedException,
