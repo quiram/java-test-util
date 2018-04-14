42d43b1c8d08c3592509523cbacd82de7e545f68
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-14 19:15:20 +0100

    add an "expectedException" assertion for methods that return void.

--- src/main/java/com/amarinperez/test_utils/Exceptions.java
+++ src/main/java/com/amarinperez/test_utils/Exceptions.java
@@ -1,5 +1,7 @@
 package com.amarinperez.test_utils;
 
+import com.amarinperez.utils.VoidCallable;
+
 import java.util.concurrent.Callable;
 
 import static org.hamcrest.core.StringContains.containsString;
@@ -8,6 +10,17 @@ import static org.junit.Assert.*;
 public class Exceptions {
     public static <T> void expectException(Callable<T> statement, Class<? extends Exception> expectedException,
                                             String expectedMessage, String failureMessage) {
+        try {
+            statement.call();
+            fail(failureMessage);
+        } catch (Exception e) {
+            assertEquals(expectedException, e.getClass());
+            assertThat(e.getMessage(), containsString(expectedMessage));
+        }
+    }
+
+    public static void expectException(VoidCallable statement, Class<? extends Exception> expectedException,
+                                       String expectedMessage, String failureMessage) {
         try {
             statement.call();
             fail(failureMessage);
