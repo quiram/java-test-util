77995435a6381031d9a36e91cf5b139aa28a6eef
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2018-04-14 20:16:39 +0100

    rename class, this doesn't apply only to constructors

--- src/main/java/com/amarinperez/test_utils/ArgumentChecks.java
+++ src/main/java/com/amarinperez/test_utils/ArgumentChecks.java
@@ -7,12 +7,12 @@ import static com.amarinperez.test_utils.Exceptions.expectException;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
 
-public class Constructors {
+public class ArgumentChecks {
     public static final List<String> BLANK_VALUES = asList(null, "", "    ");
 
-    public static <T, R> void assertIllegalArguments(Function<T, R> constructor, String field, List<T> values) {
+    public static <T, R> void assertIllegalArguments(Function<T, R> function, String field, List<T> values) {
         values.forEach(value ->
-                expectException(() -> constructor.apply(value), IllegalArgumentException.class,
+                expectException(() -> function.apply(value), IllegalArgumentException.class,
                         field, format("Exception expected when testing value '%s' for field '%s;", value, field)
                 ));
     }
