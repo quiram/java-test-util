a914ae7e42be81650f6881e07fce2d8356c9353b
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2019-01-15 11:07:27 +0000

    add expectedException methods that return the actual exception

--- src/main/java/com/github/quiram/test_utils/Exceptions.java
+++ src/main/java/com/github/quiram/test_utils/Exceptions.java
@@ -4,6 +4,7 @@ import com.github.quiram.utils.VoidCallable;
 
 import java.util.concurrent.Callable;
 
+import static java.lang.String.format;
 import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.Assert.*;
 
@@ -19,6 +20,18 @@ public class Exceptions {
         }
     }
 
+    @SuppressWarnings("unchecked")
+    public static <E extends Exception> E expectException(Callable<?> statement, Class<E> expectedException) {
+        try {
+            statement.call();
+            fail(format("Expected exception %s not thrown", expectedException.getSimpleName()));
+            return null; // Just so the compiler doesn't complain, we'll never get to this point
+        } catch (Exception e) {
+            assertEquals(expectedException, e.getClass());
+            return (E) e;
+        }
+    }
+
     public static void expectException(VoidCallable statement, Class<? extends Exception> expectedException,
                                        String expectedMessage, String failureMessage) {
         try {
@@ -27,6 +40,18 @@ public class Exceptions {
         } catch (Exception e) {
             assertEquals(expectedException, e.getClass());
             assertThat(e.getMessage(), containsString(expectedMessage));
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <E extends Exception> E expectException(VoidCallable statement, Class<E> expectedException) {
+        try {
+            statement.call();
+            fail(format("Expected exception %s not thrown", expectedException.getSimpleName()));
+            return null; // Just so the compiler doesn't complain, we'll never get to this point
+        } catch (Exception e) {
+            assertEquals(expectedException, e.getClass());
+            return (E) e;
         }
     }
 }
