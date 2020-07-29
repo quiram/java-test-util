a31f6f9c00bebe766d61f0922ba923449b614f03
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2020-07-29 17:09:42 +0100

    migreate to JUnit 5 (no need for expectedException any more)

--- src/main/java/com/github/quiram/test_utils/ArgumentChecks.java
+++ src/main/java/com/github/quiram/test_utils/ArgumentChecks.java
@@ -3,17 +3,21 @@ package com.github.quiram.test_utils;
 import java.util.List;
 import java.util.function.Function;
 
-import static com.github.quiram.test_utils.Exceptions.expectException;
 import static java.lang.String.format;
 import static java.util.Arrays.asList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ArgumentChecks {
     public static final List<String> BLANK_VALUES = asList(null, "", "    ");
 
     public static <T, R> void assertIllegalArguments(Function<T, R> function, String field, List<T> values) {
         values.forEach(value ->
-                expectException(() -> function.apply(value), IllegalArgumentException.class,
-                        field, format("Exception expected when testing value '%s' for field '%s;", value, field)
-                ));
+                {
+                    final Exception exception = assertThrows(IllegalArgumentException.class, () -> function.apply(value), format("Exception expected when testing value '%s' for field '%s;", value, field));
+                    assertThat(exception.getMessage(), containsString(field));
+                }
+        );
     }
 }
