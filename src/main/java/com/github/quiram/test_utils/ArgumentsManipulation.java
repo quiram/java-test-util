27ae764954d61dc161bd45ccabe5b3b3e3848787
Abraham Marin-Perez <abraham.marin.perez@gmail.com>
2020-08-12 15:25:11 +0100

    simplify API of ArgumentsManipulation

--- src/main/java/com/github/quiram/test_utils/ArgumentsManipulation.java
+++ src/main/java/com/github/quiram/test_utils/ArgumentsManipulation.java
@@ -6,34 +6,32 @@ import java.util.function.Function;
 import java.util.stream.IntStream;
 
 public class ArgumentsManipulation {
-    public static Function<Arguments, Arguments> extract(int i, int... indexes) {
-        return args -> extract(args, i, indexes);
+    public static Function<Arguments, Arguments> extract(int... indexes) {
+        return args -> extract(args, indexes);
     }
 
-    public static Arguments extract(Arguments arguments, int i, int... indexes) {
+    public static Arguments extract(Arguments arguments, int... indexes) {
         final Object[] args = arguments.get();
-        final Object[] newArgs = new Object[indexes.length + 1];
-        newArgs[0] = args[i];
-        IntStream.range(1, newArgs.length).forEach(j -> newArgs[j] = args[indexes[j - 1]]);
+        final Object[] newArgs = new Object[indexes.length];
+        IntStream.range(0, newArgs.length).forEach(j -> newArgs[j] = args[indexes[j]]);
 
         return Arguments.of(newArgs);
     }
 
     @SafeVarargs
-    public static Function<Arguments, Arguments> transform(Function<Object, Object> f, Function<Object, Object>... functions) {
-        return args -> transform(args, f, functions);
+    public static Function<Arguments, Arguments> transform(Function<Object, Object>... functions) {
+        return args -> transform(args, functions);
     }
 
     @SafeVarargs
-    public static Arguments transform(Arguments arguments, Function<Object, Object> f, Function<Object, Object>... functions) {
-        if (arguments.get().length != functions.length + 1) {
-            throw new IllegalArgumentException(String.format("Expected %d function(s), got %d", arguments.get().length, functions.length + 1));
+    public static Arguments transform(Arguments arguments, Function<Object, Object>... functions) {
+        if (arguments.get().length != functions.length) {
+            throw new IllegalArgumentException(String.format("Expected %d function(s), got %d", arguments.get().length, functions.length));
         }
 
         final Object[] args = arguments.get();
-        final Object[] newArgs = new Object[functions.length + 1];
-        newArgs[0] = f.apply(args[0]);
-        IntStream.range(1, newArgs.length).forEach(j -> newArgs[j] = functions[j - 1].apply(args[j]));
+        final Object[] newArgs = new Object[functions.length];
+        IntStream.range(0, newArgs.length).forEach(j -> newArgs[j] = functions[j].apply(args[j]));
 
         return Arguments.of(newArgs);
 
